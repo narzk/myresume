d89d7be1639e3c2bd243b57752d24bb1d73bac29
narzk <niloofararazkhani@gmail.com>
2019-10-20 13:31:11 +0330

    Updates

---
+++ precache-manifest.3b6bcd216de8be0dadb8d4b5f318315d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f60e4baa2aeac8ed9f48703f3572c37",
+    "url": "/myresume/index.html"
+  },
+  {
+    "revision": "611f55f93004fa5568ac",
+    "url": "/myresume/static/css/main.0661879e.chunk.css"
+  },
+  {
+    "revision": "f0e5512a563fa824929e",
+    "url": "/myresume/static/js/2.64ca5ed3.chunk.js"
+  },
+  {
+    "revision": "611f55f93004fa5568ac",
+    "url": "/myresume/static/js/main.2f57a08e.chunk.js"
+  },
+  {
+    "revision": "412d1617c022c1d0551e",
+    "url": "/myresume/static/js/runtime-main.26cf8fed.js"
+  }
+]);
